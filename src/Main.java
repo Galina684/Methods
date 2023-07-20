6f1a649d83419fba26c25509592e9eebfa1c01bf
Galina684 <galina684@gmail.com>
2023-07-21 00:15:50 +0300

    задачи 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,55 @@
 import java.nio.file.attribute.UserPrincipalLookupService;
 
 public class Main {
-    public static int identifyLeapYear(int i) {
+    public static void identifyLeapYear(int i) {
         if ((i % 100 != 0) && (i % 4 == 0) || (i % 400 == 0)) {
             System.out.println(i + " год - високосный");
         } else {
             System.out.println(i + " год - не високосный");
         }
-        return i;
     }
 
-    public static String selectApplication(String name) {
-        if (name.equals("iOS")) {
-            System.out.println("Установите приложение для Android");
-
-        } else {
-            System.out.println("Установите приложение для iOS");
-
+    public static void selectApplication(int year, int clientOs) {
+        switch (clientOs) {
+            case 0:
+                if (year < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("установите приложение для iOs по ссылке");
+                }
+                break;
+            case 1:
+                if (year < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("установите приложение для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("Тип операционной системы не определен");
+                break;
         }
-        return "";
+    }
+
+    public static int deliverCards(int deliveryDistance) {
+        int time = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Срок доставки " + time + " день");
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Срок доставки " + (time + 1) + " дня");
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Срок доставки " + (time + 2) + " дня");
+        } else if (deliveryDistance >= 100) {
+            System.out.println("По вашему адресу доставка не осуществляется");
+        }
+        return time;
+
     }
 
     public static void main(String[] args) {
         task1();
         task2();
-        //task3();
+        task3();
     }
 
     public static void task1() {
@@ -35,15 +60,16 @@ public class Main {
 
     public static void task2() {
         System.out.println(" Задача 2");
+        int clientOs = 0;
+        int clientDeviceYear = 2014;
 
-
-        String clientOs = "Android";
-
-
-        selectApplication(clientOs);
+        selectApplication(clientDeviceYear, clientOs);
 
     }
-    //public static void task3() {
 
-    //}
+    public static void task3() {
+        System.out.println(" Задача 3");
+        int deliveryDistance = 95;
+        deliverCards(deliveryDistance);
     }
+}
